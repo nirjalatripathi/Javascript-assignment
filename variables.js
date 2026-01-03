2d583cc609efa777eb88776573a6cb6fd79dda13
nirjalatripathi <nirjalatripathi41@gmail.com>
2026-01-03 13:11:54 +0545

    Initial commit (JS Assignment)

---
+++ variables.js
@@ -0,0 +1,28 @@
+//Q.1.
+let name="Nirjala Tripathi";
+let age="19";
+let isStudent="yes";
+console.log (name, age, isStudent);
+
+//Q.2. 
+let num= 50;
+console.log ("before:", num);
+num=100;
+console.log ("after:", num);
+
+//Q.3.
+let a=50, b=100;
+[a,b] = [b,a];
+console.log ("swapped values are:", a, b);
+
+//Q.4
+const PI= 3.1415;
+console.log ("value of pi is ", PI);
+//const PI=3.5;
+//console.log ("value of pi is"; PI);
+// If we try to reassign the value of PI again it causes an error because we can't reassign a const.  
+
+
+//Q.4.
+let favouriteColor = "White";
+console.log ("My favourite Color is", favouriteColor);
